7368813d30c45338da027972952691cc27fddc84
jake waters <jerkey@g-cipher.net>
2015-05-01 17:56:24 -0700

    put in detailed definition of input CSV

--- digiconv.py
+++ digiconv.py
@@ -1,21 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import csv
 import sys
 
-#
-# entries
-#
-# 0 URL
-# 2 ?
-# 3 ?
-# 5
-# 8 ?
-# 10
-# 13 Capacitance
-# 15 (5V)
-# 16 (300 mOhm)
-# 21
-# 22
-# 23
+#  0 Datasheets                         "http://www.cooperindustries.com/content/dam/public/bussmann/Electronics/Resources/product-datasheets/Bus_Elx_DS_4308_PM_Series.pdf",
+#  1 Image                              "http://media.digikey.com/Photos/Cooper%20Bussmann%20Photos/PM-5R0H474-R.JPG",
+#  2 Digi-Key Part Number               "283-4004-ND",
+#  3 Manufacturer Part Number           "PM-5R0H474-R",
+#  4 Manufacturer                       "Eaton Bussmann",
+#  5 Description                        "CAP 470MF -20% +80% 5V T/H",
+#  6 Quantity Available                 42,
+#  7 Factory Stock                      0,
+#  8 Unit Price (USD)                   "7.31000",
+#  9 @ qty                              0,
+# 10 Minimum Quantity                   1,
+# 11 Packaging                          "Bulk",
+# 12 Series                             "PowerStor® PM",
+# 13 Capacitance                        "470mF",
+# 14 Tolerance                          "-20%, +80%",
+# 15 Voltage - Rated                    "5V",
+# 16 ESR (Equivalent Series Resistance) "500 mOhm",
+# 17 Lifetime @ Temp.                   "1000 Hrs @ 60°C",
+# 18 Termination                        "-",
+# 19 Mounting Type                      "Through Hole",
+# 20 Package / Case                     "Radial, Can, Horizontal",
+# 21 Lead Spacing                       "0.465"" (11.80mm)",
+# 22 Size / Dimension                   "0.571"" L x 0.681"" W (14.50mm x 17.30mm)",
+# 23 Height - Seated (Max)              "0.354"" (9.00mm)",
+# 24 Operating Temperature              "-40°C ~ 60°C"
 
 def extract_paren(s):
     start = s.find('(')
